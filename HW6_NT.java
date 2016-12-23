c5f8e44f43534ab3f9e8c044c3a4abbd15d839c3
TNathen <truong.nathen1213@gmail.com>
2016-12-23 13:58:20 -0500

    Removed unused line

--- HW6_NT.java
+++ HW6_NT.java
@@ -46,7 +46,6 @@ public class HW6_NT extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         //Creates Menu Bar
         menu = new MenuBar();
